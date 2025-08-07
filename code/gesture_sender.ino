ddd7c5111a31c00364f27664b80b2720d13c11c0
syhirmcc <syhirmcclelland@gmail.com>
2025-08-06 23:03:00 -0400

    Update gesture_sender.ino

--- code/gesture_sender.ino
+++ code/gesture_sender.ino
@@ -1,1 +1,10 @@
+// gesture_sender.ino
+// Placeholder for ESP32 IMU gesture transmitter
 
+void setup() {
+  // TODO: Add MPU6050 init and WiFi send
+}
+
+void loop() {
+  // TODO: Read IMU and send data wirelessly
+}
